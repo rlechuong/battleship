505a784d543f0266bac33b4aa1f6a1026d265d8f
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-08-26 20:38:14 -0400

    Refactor test structure slightly

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -2,63 +2,69 @@ import { describe, test, expect } from "@jest/globals";
 import { Ship } from "./Ship.js";
 
 describe("Ship", () => {
-  test("should create a ship with specified length", () => {
-    const ship = new Ship(3);
-    expect(ship.length).toBe(3);
+  describe("creation", () => {
+    test("should create a ship with specified length", () => {
+      const ship = new Ship(3);
+      expect(ship.length).toBe(3);
+    });
+
+    test("should create a ship with a different specified length", () => {
+      const ship = new Ship(6);
+      expect(ship.length).toBe(6);
+    });
   });
 
-  test("should create a ship with a different specified length", () => {
-    const ship = new Ship(6);
-    expect(ship.length).toBe(6);
+  describe("hit", () => {
+    test("new ship should start with 0 hits", () => {
+      const ship = new Ship(3);
+      expect(ship.hits).toBe(0);
+    });
+
+    test("hit() should increase hits by 1", () => {
+      const ship = new Ship(3);
+      expect(ship.hits).toBe(0);
+      ship.hit();
+      expect(ship.hits).toBe(1);
+    });
+
+    test("hit() should increase hits by 1 each call", () => {
+      const ship = new Ship(3);
+      expect(ship.hits).toBe(0);
+      ship.hit();
+      expect(ship.hits).toBe(1);
+      ship.hit();
+      expect(ship.hits).toBe(2);
+    });
   });
 
-  test("new ship should start with 0 hits", () => {
-    const ship = new Ship(3);
-    expect(ship.hits).toBe(0);
-  });
+  describe("isSunk()", () => {
+    test("new ship should not be sunk", () => {
+      const ship = new Ship(3);
+      expect(ship.isSunk()).toBe(false);
+    });
 
-  test("hit() should increase hits by 1", () => {
-    const ship = new Ship(3);
-    expect(ship.hits).toBe(0);
-    ship.hit();
-    expect(ship.hits).toBe(1);
-  });
+    test("ship with less hits than length not be sunk", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).toBe(false);
+    });
 
-  test("hit() should increase hits by 1 each call", () => {
-    const ship = new Ship(3);
-    expect(ship.hits).toBe(0);
-    ship.hit();
-    expect(ship.hits).toBe(1);
-    ship.hit();
-    expect(ship.hits).toBe(2);
-  });
+    test("ship with equal hits and length should be sunk", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).toBe(true);
+    });
 
-  test("new ship should not be sunk", () => {
-    const ship = new Ship(3);
-    expect(ship.isSunk()).toBe(false);
-  });
-
-  test("ship with less hits than length not be sunk", () => {
-    const ship = new Ship(3);
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
-  });
-
-  test("ship with equal hits and length should be sunk", () => {
-    const ship = new Ship(3);
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
-  });
-
-  test("ship with greater hits than length should be sunk", () => {
-    const ship = new Ship(3);
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
+    test("ship with greater hits than length should be sunk", () => {
+      const ship = new Ship(3);
+      ship.hit();
+      ship.hit();
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).toBe(true);
+    });
   });
 });
