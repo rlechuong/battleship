7b08ee54c2eaf564de74277fdede7198ce092f7a
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-09-02 00:31:12 -0400

    Add computerAttack() and tests

--- src/Player.js
+++ src/Player.js
@@ -5,6 +5,23 @@ class Player {
     this.type = type;
     this.gameboard = new Gameboard();
   }
+
+  computerAttack(opponentBoard) {
+    if (opponentBoard.misses.size + opponentBoard.hits.size === 100) {
+      return false;
+    }
+
+    let attackResult;
+
+    do {
+      const randomRow = Math.floor(Math.random() * 10);
+      const randomColumn = Math.floor(Math.random() * 10);
+
+      attackResult = opponentBoard.receiveAttack([randomRow, randomColumn]);
+    } while (attackResult !== true && attackResult !== false);
+
+    return true;
+  }
 }
 
 export { Player };
--- src/Player.test.js
+++ src/Player.test.js
@@ -1,6 +1,5 @@
 import { describe, test, expect } from "@jest/globals";
 import { Gameboard } from "./Gameboard.js";
-import { Ship } from "./Ship.js";
 import { Player } from "./Player.js";
 
 describe("Player", () => {
@@ -17,6 +16,45 @@ describe("Player", () => {
       expect(computerPlayer.type).toBe("computer");
       expect(computerPlayer).toHaveProperty("gameboard");
       expect(computerPlayer.gameboard).toBeInstanceOf(Gameboard);
+    });
+  });
+
+  describe("computer attack", () => {
+    test("computer should attack opponent's board", () => {
+      const realPlayer = new Player("real");
+      const computerPlayer = new Player("computer");
+
+      expect(computerPlayer.computerAttack(realPlayer.gameboard)).toBe(true);
+    });
+
+    test("computer should attack board if one square remains", () => {
+      const realPlayer = new Player("real");
+      const computerPlayer = new Player("computer");
+
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 9; j++) {
+          realPlayer.gameboard.receiveAttack([i, j]);
+        }
+      }
+
+      for (let i = 0; i < 9; i++) {
+        realPlayer.gameboard.receiveAttack([i, 9]);
+      }
+
+      expect(computerPlayer.computerAttack(realPlayer.gameboard)).toBe(true);
+    });
+
+    test("computer should fail to attack board if no squares remain", () => {
+      const realPlayer = new Player("real");
+      const computerPlayer = new Player("computer");
+
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          realPlayer.gameboard.receiveAttack([i, j]);
+        }
+      }
+
+      expect(computerPlayer.computerAttack(realPlayer.gameboard)).toBe(false);
     });
   });
 });
